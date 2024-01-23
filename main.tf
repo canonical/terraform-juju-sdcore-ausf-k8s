79987290b67c553ad3cf125a02e429a83635cfdb
gatici <gulsum.atici@canonical.com>
2024-01-23 14:34:29 +0300

    Initial commit
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "juju_application" "ausf" {
+  name = "ausf"
+  model = var.model_name
+
+  charm {
+    name = "sdcore-ausf-k8s"
+    channel = var.channel
+  }
+
+  units = 1
+  trust = true
+}
+
+resource "juju_integration" "ausf-db" {
+  model = var.model_name
+
+  application {
+    name     = juju_application.ausf.name
+    endpoint = "database"
+  }
+
+  application {
+    name     = var.db_application_name
+    endpoint = "database"
+  }
+}
+
+resource "juju_integration" "ausf-certs" {
+  model = var.model_name
+
+  application {
+    name     = juju_application.ausf.name
+    endpoint = "certificates"
+  }
+
+  application {
+    name     = var.certs_application_name
+    endpoint = "certificates"
+  }
+}
+
+resource "juju_integration" "ausf-nrf" {
+  model = var.model_name
+
+  application {
+    name     = juju_application.ausf.name
+    endpoint = "fiveg-nrf"
+  }
+
+  application {
+    name     = var.nrf_application_name
+    endpoint = "fiveg-nrf"
+  }
+}
